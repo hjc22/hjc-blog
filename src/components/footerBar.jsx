7c915945df6bdb6562cdac5c2f5c47c9fdbf1124
huangjingchao <huangjingchao-cf@matorsc.com>
2018-04-03 11:38:53 +0800

    Up

--- src/components/footerBar.jsx
+++ src/components/footerBar.jsx
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 import { observer } from 'mobx-react';
 import { countStore } from '../stores'
 import cs from '../assets/css/footerbar.css'
+import { Icon } from 'antd'
 
 @observer
 class FooterBar extends Component {
@@ -13,9 +14,10 @@ class FooterBar extends Component {
    render(){
      return (
        <div className={cs.footerBar}>
-          <p>博客以运行1000天</p>
+          <p>博客已运行1000天</p>
           <p>托管于阿里云</p>
           <p>Made with by hjc</p>
+          <p><a href='https://github.com/hjc22' target='_blank'><Icon type="github" className={cs.github}/></a></p>
        </div>
      )
    }
