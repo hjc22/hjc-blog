b90a34bbe0a29f44b21564f0e437525b92311212
huangjingchao <huangjingchao-cf@matorsc.com>
2018-06-21 11:23:39 +0800

    222

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,34 @@
-import Index from './start.jsx'
-import Order from './order.jsx'
-import Login from './login.jsx'
-import Article from './article.jsx'
-import ErrorPage from './error.jsx'
-import Edit from './editArticle.jsx'
-import MySetting from './mySetting'
-import MessagePage from './message'
-import MessageBoard from './messageBoard'
-import Tag from './tag'
-import About from './about'
-import Edit2 from './edit2'
+import React, {Component} from 'react'
+import Loadable from 'react-loadable'
+import {Spin} from 'antd'
+import cs from '../assets/css/loading.css'
+
+// import Index from './start.jsx'
+// import Order from './order.jsx'
+// import Login from './login.jsx'
+// // import Article from './article.jsx'
+// import ErrorPage from './error.jsx'
+// import Edit from './editArticle.jsx'
+// // import MySetting from './mySetting'
+// import MessagePage from './message'
+// import MessageBoard from './messageBoard'
+// import Tag from './tag'
+// import About from './about'
+// import Edit2 from './edit2'
+
+const Index = asyncComponent(() => import('./start.jsx'))
+const Article = asyncComponent(() => import('./article.jsx'))
+const MySetting = asyncComponent(() => import('./mySetting.jsx'))
+const Order = asyncComponent(() => import('./order.jsx'))
+const Login = asyncComponent(() => import('./login.jsx'))
+const ErrorPage = asyncComponent(() => import('./error.jsx'))
+const Edit = asyncComponent(() => import('./editArticle.jsx'))
+const MessagePage = asyncComponent(() => import('./message.jsx'))
+const MessageBoard = asyncComponent(() => import('./messageBoard.jsx'))
+const Tag = asyncComponent(() => import('./tag.jsx'))
+const About = asyncComponent(() => import('./about.jsx'))
+const Edit2 = asyncComponent(() => import('./edit2.jsx'))
+
 export {
   Index,
   Order,
@@ -23,4 +42,22 @@ export {
   Tag,
   About,
   Edit2
+}
+
+
+
+function asyncComponent(importComponent) {
+  return Loadable({
+    loader: importComponent,
+    loading:Loading,
+  })
+}
+
+function Loading(isLoading) {
+
+  return (
+    <div className={cs.loading+' '+cs.loadingWhite}>
+        <Spin className={cs.spinStyle}></Spin>
+    </div>
+  )
 }
